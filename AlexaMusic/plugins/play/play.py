e8567a78df8482e53748b962537ed5c830b518b5
iamnolimit <manonebillion@gmail.com>
2025-10-22 10:14:21 +0700

    Remove unused import for channelplay in play module

--- AlexaMusic/plugins/play/play.py
+++ AlexaMusic/plugins/play/play.py
@@ -17,7 +17,6 @@ import config
 from AlexaMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
 from AlexaMusic.core.call import Alexa
 from AlexaMusic.utils import seconds_to_min, time_to_seconds
-from AlexaMusic.utils.channelplay import get_channeplayCB
 from AlexaMusic.utils.database import is_video_allowed
 from AlexaMusic.utils.decorators.language import languageCB
 from AlexaMusic.utils.decorators.play import PlayWrapper
